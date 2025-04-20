37ab8b21f9940b3a45103364aa72de3d5d69a3c4
ᒎᗝᔕᗴ ₪亗🥀 <jr3303361587@hotmail.com>
2025-04-20 10:57:30 -0300

    adiciona módulos para manipulação de valores e formatação de texto no terminal

--- #22 Módulos e Pacotes/Ex107/Color.py
+++ #22 Módulos e Pacotes/Ex107/Color.py
@@ -1,8 +1,3 @@
-from rich.console import Console
-
-console = Console()
-
-console.print("[bold magenta]Hello[/bold magenta] [green]World![/green] :sparkles:")
 def c(color=""):
     """ --> Função colorir terminal\n
         1 - vermelho| 5 - roxo\n       
@@ -35,8 +30,3 @@ def caixa_arredondada(texto, cor_texto=37, cor_fundo=40):
     borda_superior = f"{c(cor_fundo)}╭{'─' * largura}╮{c()}"
     borda_inferior = f"{c(cor_fundo)}╰{'─' * largura}╯{c()}"
     return f"{borda_superior}\n{c(cor_fundo)}│{texto_formatado}│{c()}\n{borda_inferior}"
-
-# Exemplo de uso
-if __name__ == "__main__":
-    print(caixa_arredondada("José Roberto", cor_texto=33))
-    print(f"{c()}Oiee{c()}")
